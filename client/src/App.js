bc21836d2447d997678973b29d468934506e9412
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-04-01 08:56:31 -0500

    ran create-react-app to build frontend

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Planterest!</h1>
+      <h3>
+        An image board site for plant-lovers to curate boards with images of
+        their favorite flora
+      </h3>
+      <h4>Come on in, the air is fresh in here!</h4>
+    </div>
+  );
+}
+
+export default App;
